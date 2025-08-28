f7ed321c42917c2072e55456cde193fa76ff3dbf
amandaliram <130415646+amandaliram@users.noreply.github.com>
2025-08-28 02:39:29 +0000

    add arquivos

---
+++ index.js
@@ -0,0 +1,10 @@
+console.log("\n Demonstrando Hashing...");
+require('./hashing');
+setTimeout(() => {
+ console.log("\n Demonstrando Criptografia Simétrica...");
+ require('./simetrica');
+}, 2000);
+setTimeout(() => {
+ console.log("\n Demonstrando Criptografia Assimétrica...");
+ require('./assimetrica');
+}, 4000);
